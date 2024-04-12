254fe9d76c9c0e8571960c45bc050fe80e519402
1smex48 <i.flor@sapalomera.cat>
2024-04-12 16:43:08 +0200

    roles

--- SECURITZACIÓ DE BD/privilege_roles.sql
+++ SECURITZACIÓ DE BD/privilege_roles.sql
@@ -4,14 +4,16 @@ CREATE ROLE mediquito;
 CREATE ROLE enfermerito;
 CREATE ROLE celadorito;
 CREATE ROLE recepcionistito;
-CREATE ROLE administradorcito;
+CREATE ROLE administradorcito_informatiquito;
+CREATE ROLE administradorcito_hospitalito;
 CREATE ROLE conductorAmbulancito;
 
 GRANT CONNECT ON DATABASE hospitalito TO mediquito;
 GRANT CONNECT ON DATABASE hospitalito TO enfermerito;
 GRANT CONNECT ON DATABASE hospitalito TO celadorito;
 GRANT CONNECT ON DATABASE hospitalito TO recepcionistito;
-GRANT CONNECT ON DATABASE hospitalito TO administradorcito;
+GRANT CONNECT ON DATABASE hospitalito TO administradorcito_informatiquito;
+GRANT CONNECT ON DATABASE hospitalito TO administradorcito_hospitalito;
 GRANT CONNECT ON DATABASE hospitalito TO conductorAmbulancito;
 
 GRANT USAGE ON SCHEMA public TO mediquito;
@@ -36,4 +38,14 @@ GRANT SELECT ON TABLE reserva_habitacion IN SCHEMA public TO enfermerito;
 GRANT SELECT ON TABLE enfermeros_asignados_medico IN SCHEMA public TO enfermerito;
 GRANT SELECT nombre, apellidos, condiciones_paciente ON TABLE pacientes IN SCHEMA public TO enfermerito;
 
-GRANT SELECT 
+GRANT SELECT ON TABLE reserva_habitacion IN SCHEMA public TO celadorito;
+GRANT SELECT ON TABLE habitaciones IN SCHEMA public TO celadorito;
+GRANT SELECT nombre, apellidos, condiciones_paciente ON TABLE pacientes IN SCHEMA public TO celadorito;
+
+GRANT SELECT ON TABLE pacientes IN SCHEMA public TO recepcionistito;
+GRANT SELECT ON TABLE habitaciones IN SCHEMA public TO recepcionistito;
+GRANT SELECT ON TABLE reserva_habitacion IN SCHEMA public TO recepcionistito;
+
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO administradorcito_informatiquito;
+
+GRANT ALL PRIVILEGES ON TABLE personal IN SCHEMA public TO administradorcito_hospitalito;
