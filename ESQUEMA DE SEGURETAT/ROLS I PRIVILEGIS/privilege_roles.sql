0ffefab96fea7f93c889bcf0c9b4cddd57f78210
1smex48 <i.flor@sapalomera.cat>
2024-04-18 18:57:24 +0200

    Update privilege_roles.sql

--- ESQUEMA DE SEGURETAT/ROLS I PRIVILEGIS/privilege_roles.sql
+++ ESQUEMA DE SEGURETAT/ROLS I PRIVILEGIS/privilege_roles.sql
@@ -1,5 +1,3 @@
--- Active: 1712759496762@@10.94.255.109@5432@hospitalito@
-
 CREATE ROLE medico;
 CREATE ROLE enfermero;
 CREATE ROLE celador;
@@ -16,36 +14,40 @@ GRANT CONNECT ON DATABASE hospital TO administrador_informatico;
 GRANT CONNECT ON DATABASE hospital TO administrador_hospital;
 GRANT CONNECT ON DATABASE hospital TO conductor_ambulancia;
 
-GRANT USAGE ON SCHEMA  TO medico;
-GRANT USAGE ON SCHEMA  TO enfermero;
-GRANT USAGE ON SCHEMA  TO celador;
-GRANT USAGE ON SCHEMA  TO administradorcito;
-GRANT USAGE ON SCHEMA  TO conductor_ambulancia;
+GRANT USAGE ON SCHEMA public TO medico;
+GRANT USAGE ON SCHEMA public TO enfermero;
+GRANT USAGE ON SCHEMA public TO celador;
+GRANT USAGE ON SCHEMA public TO recepcionista;
+GRANT USAGE ON SCHEMA public TO administrador_informatico;
+GRANT USAGE ON SCHEMA public TO administrador_hospital;
+GRANT USAGE ON SCHEMA public TO conductor_ambulancia;
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE diagnosticos  TO medico;
-GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE operaciones  TO medico;
-GRANT SELECT ON TABLE quirofanos  TO medico;
-GRANT SELECT ON TABLE habitaciones  TO medico;
-GRANT SELECT ON TABLE reserva_habitacion  TO medico;
-GRANT SELECT ON TABLE enfermeros_asignados_medico  TO medico;
-GRANT SELECT(nombre, apellidos, condiciones_paciente) ON TABLE pacientes  TO medico;
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE diagnosticos TO medico;
+GRANT SELECT, INSERT, UPDATE,DELETE ON TABLE operacion TO medico;
+GRANT SELECT ON TABLE quirofano TO medico;
+GRANT SELECT ON TABLE habitaciones TO medico;
+GRANT SELECT ON TABLE reservas TO medico;
+GRANT SELECT ON TABLE medico_enfermeria TO medico;
+GRANT SELECT(nombre, apellidos, condiciones_paciente) ON TABLE pacientes TO medico;
 
-GRANT SELECT ON TABLE diagnosticos  TO enfermero;
-GRANT SELECT ON TABLE operaciones  TO enfermero;
-GRANT SELECT ON TABLE quirofanos  TO enfermero;
-GRANT SELECT ON TABLE habitaciones  TO enfermero;
-GRANT SELECT ON TABLE reserva_habitacion   TO enfermero;
-GRANT SELECT ON TABLE enfermeros_asignados_medico   TO enfermero;
+GRANT SELECT ON TABLE diagnosticos TO enfermero;
+GRANT SELECT ON TABLE operacion TO enfermero;
+GRANT SELECT ON TABLE quirofano TO enfermero;
+GRANT SELECT ON TABLE habitaciones TO enfermero;
+GRANT SELECT ON TABLE reservas TO enfermero;
+GRANT SELECT ON TABLE medico_enfermeria TO enfermero;
 GRANT SELECT(nombre, apellidos, condiciones_paciente) ON TABLE pacientes TO enfermero;
 
-GRANT SELECT ON TABLE reserva_habitacion   TO celador;
-GRANT SELECT ON TABLE habitaciones   TO celador;
+GRANT SELECT ON TABLE reservas TO celador;
+GRANT SELECT ON TABLE habitaciones TO celador;
 GRANT SELECT(nombre, apellidos, condiciones_paciente) ON TABLE pacientes TO celador;
 
-GRANT SELECT ON TABLE pacientes   TO recepcionista;
-GRANT SELECT ON TABLE habitaciones   TO recepcionista;
-GRANT SELECT ON TABLE reserva_habitacion   TO recepcionista;
+GRANT SELECT ON TABLE pacientes TO recepcionista;
+GRANT SELECT ON TABLE habitaciones TO recepcionista;
+GRANT SELECT ON TABLE reservas TO recepcionista;
 
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO administrador_informatico;
 
-GRANT ALL PRIVILEGES ON TABLE personal  TO administrador_hospital;
+GRANT SELECT ON TABLE personal TO administrador_hospital;
+
+GRANT SELECT, INSERT ON TABLE reservas TO conductor_ambulancia;
