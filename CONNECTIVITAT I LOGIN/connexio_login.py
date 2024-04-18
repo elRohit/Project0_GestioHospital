e21ed4b0c34f80b948c47ea3367e37d148d4be9e
1smex48 <i.flor@sapalomera.cat>
2024-04-18 17:59:30 +0200

    roles

--- CONNECTIVITAT I LOGIN/connexio_login.py
+++ CONNECTIVITAT I LOGIN/connexio_login.py
@@ -1,198 +1,67 @@
+# En mantenimiento
+
 import psycopg2
     
 def loginito(usuarito, contrasenyita):
     
     try:
         connexio = psycopg2.connect(
-            dbname="hospitalito",
+            dbname="hospital",
             user=usuarito,
             password=contrasenyita,
-            host="10.94.255.109",
-            port="5432"
+            host="10.94.255.129",
+            port="5432",
+            sslmode="require"
         )
         if connexio:
             return True
     except psycopg2.Error as e:
         print(f"Error en la conexión: {e}")
         return False
-    
-def mainQueryta(usuarito, tablitaAfectadita):
-    print("+----------------------------+")
-    print("|      ¿Que desea hacer?     |")
-    print("+----------------------------+")
-    permisitos(usuarito, tablitaAfectadita)
-    print("+----------------------------+")
+    finally:
+        connexio.close()
 
-def permisitos(usuarito, tablitaAfectadita):
+def enQueRolsitoEsta(usuarito):
     
     connexio = psycopg2.connect(
-        dbname="hospitalito",
+        dbname="hospital",
         user="postgres",
         password="P@ssw0rd",
-        host="10.94.255.109",
-        port="5432"
+        host="10.94.255.129",
+        port="5432",
+        sslmode="require"
         )
     
-    SQLita = f"SELECT privilege_type FROM information_schema.table_privileges WHERE grantee = '{usuarito}' AND is_grantable = 'YES' AND table_catalog = 'hospitalito' AND table_name = '{tablitaAfectadita}';"
     cur = connexio.cursor()
+    SQLita = f"SELECT r.rolname AS usuario, r1.rolname AS rol FROM pg_catalog.pg_roles r JOIN pg_catalog.pg_auth_members m ON (m.member = r.oid) JOIN pg_roles r1 ON (m.roleid = r1.oid) WHERE r.rolcanlogin AND r.rolname = '{usuarito}' ORDER BY 1;"
     cur.execute(SQLita)
-    rows = cur.fetchall()
+    resultadito = cur.fetchall()
     cur.close()
     connexio.close()
-     
-    for row in rows:
-        if row == ('INSERT',):
-            print(f"{"1.- Insertar datos":<29}|")
-        elif row == ('SELECT',):
-            print(f"{"2.- Consultar datos":<29}|")
-        elif row == ('UPDATE',):
-            print(f"{"3.- Actualizar datos":<29}|")
-        elif row == ('DELETE',):
-            print(f"{"4.- Borrar datos":<29}|")
-        elif row == ('REFERENCES',):
-            print(f"{"5.- Referenciar datos":<29}|")
-        elif row == ('TRIGGER',):
-            print(f"{"6.- Crear triggers":<29}|")
-
-def insertarDatitos(usuarito,contrasenyita, tablitaAfectadita):
-    connexio = psycopg2.connect(
-        dbname="hospitalito",
-        user=usuarito,
-        password=contrasenyita,
-        host="10.94.255.109",
-        port="5432"
-    )
-    cur = connexio.cursor()
-    cur.execute(f"SELECT column_name FROM information_schema.columns WHERE table_name = '{tablitaAfectadita}';")
-        
-    listitaAnyaditos = []
-    rows = cur.fetchall()
-    for row in rows:
-        listitaAnyaditos.append(input(f"Introduce el valor de {row[0]}: "))
-            
-    cur.execute(f"INSERT INTO {tablitaAfectadita} VALUES {tuple(listitaAnyaditos)};")
-    print("Datos insertados correctamente")
-    print(f"Has insertado los siguientes datos: {listitaAnyaditos}")
-    connexio.commit()
-        
-    cur.close()
-    connexio.close()
-        
-def consultarDatitos(usuarito,contrasenyita, tablitaAfectadita):
-    connexio = psycopg2.connect(
-        dbname="hospitalito",
-        user=usuarito,
-        password=contrasenyita,
-        host="10.94.255.109",
-        port="5432"
-    )
-    cur = connexio.cursor()
-    cur.execute(f"SELECT column_name FROM information_schema.columns WHERE table_name = '{tablitaAfectadita}';")
-        
-    print("Los campos de la tabla son: ")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row[0])
-            
-    tuConsultitaCampitos = input("Introduce los campos que deseas realizar (separalos con ','): ").split(",")
-        
-    quiereCondicioncita = input("¿Desea añadir una condición? (s/n): ")
-    if quiereCondicioncita.lower() == "s":
-        condicioncita = input("Introduce la condición/es (ponga <campo> = <condición> <AND> ...): ")
-            
-    quiereOrdencito = input("¿Desea ordenar los datos? (s/n): ")
-    if quiereOrdencito.lower() == "s":
-        ordencito = input("Introduce el campo de orden y si es ascendente/descendente (ponga <campo> ASC/DESC):")
-        
-    if quiereCondicioncita == "s" and quiereOrdencito == "s":
-        cur.execute(f"SELECT {','.join(tuConsultitaCampitos)} FROM {tablitaAfectadita} WHERE {condicioncita} ORDER BY {ordencito};")
-            
-    elif quiereCondicioncita == "s" and quiereOrdencito == "n":
-        cur.execute(f"SELECT {','.join(tuConsultitaCampitos)} FROM {tablitaAfectadita} WHERE {condicioncita};")
-            
-    elif quiereCondicioncita == "n" and quiereOrdencito == "s":
-        cur.execute(f"SELECT {','.join(tuConsultitaCampitos)} FROM {tablitaAfectadita} ORDER BY {ordencito};")
-            
-    elif quiereCondicioncita == "n" and quiereOrdencito == "n":
-        cur.execute(f"SELECT {','.join(tuConsultitaCampitos)} FROM {tablitaAfectadita};")
-    connexio.commit()
-        
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)        
-       
-    cur.close()
-    connexio.close()
- 
-def actualizarDatitos(usuarito,contrasenyita, tablitaAfectadita): 
-    connexio = psycopg2.connect(
-        dbname="hospitalito",
-        user=usuarito,
-        password=contrasenyita,
-        host="10.94.255.109",
-        port="5432"
-    )
-    cur = connexio.cursor()
-    cur.execute(f"SELECT column_name FROM information_schema.columns WHERE table_name = '{tablitaAfectadita}';")
+    rol = str(resultadito[0][1])
+    return rol
     
-    print("Los campos de la tabla son: ")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row[0])
-    
-    eleccionita = input("Introduce los campos que desea modificar (separelos con ','): ").split(",")
-    condiciones = input("Introduce la condición campo = condicion (separelos con AND si es más de una condición): ").split("AND")
-    
-    for i in range(len(condiciones)):
-        condiciones[i] = condiciones[i].strip()
-    
-    print(condiciones)
-    
-    print("Introduce los nuevos valores (separelos con ',' y en orden): ")
-    nuevosValores = input().split(",") 
-    
-    for i in range(len(nuevosValores)):
-        nuevosValores[i] = nuevosValores[i].strip()
-        
-    print(nuevosValores)
-    
-    cur.execute(f"UPDATE {tablitaAfectadita} SET {','.join(eleccionita)} = {tuple(nuevosValores)} WHERE {' AND '.join(condiciones)};")
-        
-    cur.close()
-    connexio.close()
-    
-
-def queQuiereHacerito(usuarito,contrasenyita, eleccionita, tablitaAfectadita):
-    
-    if eleccionita == 1:
-        insertarDatitos(usuarito,contrasenyita, tablitaAfectadita)
-            
-    elif eleccionita == 2:
-        consultarDatitos(usuarito,contrasenyita, tablitaAfectadita)
-        
-    elif eleccionita == 3:
-        actualizarDatitos(usuarito,contrasenyita, tablitaAfectadita)
-    elif eleccionita == 4:
-        pass
-    elif eleccionita == 5:
-        pass
-    elif eleccionita == 6:
-        pass
+def menuPorRol(rol):
+    if rol == "administrador":
+        print("+----------------------------------------+")
+        print("| 1. Dar de alta un nuevo usuario        |")   
+        print("| 2. Dar de baja un usuario existente    |")
+        print("| 3. Dar de alta un nuevo paciente       |")
+        print("| 4. Dar de baja un paciente existente   |")
+        print("| 5. Consultar usuarios existentes       |")
+        print("+----------------------------------------+")
+    if rol == "medico":
+        print("| 1. Consultar historial de un paciente  |")
 
 
 def main_connexio():
     usuarito = input("Introduce el nombre de usuario: ")
     contrasenyita = input("Introduce la contraseña: ")
-    tablitaAfectadita = input("Introduce la tabla a la cual quiere efectuar un comando: ")
     if loginito(usuarito, contrasenyita) == True:
         print("+----------------------------+")
         print("Conexión establecida con éxito")
-        prosigamitos = True
-        while prosigamitos == True:
-            mainQueryta(usuarito, tablitaAfectadita)
-            eleccionita = int(input("Introduce el número de la acción: "))
-            queQuiereHacerito(usuarito, contrasenyita, eleccionita, tablitaAfectadita)
-            quiereContinuaarito = input("¿Desea continuar? (s/n): ")
-            if quiereContinuaarito.lower() == "n":
-                prosigamitos = False
-    
+        rolecitos = enQueRolsitoEsta(usuarito)
+        menuPorRol(rolecitos)
+        
+        
+main_connexio()
