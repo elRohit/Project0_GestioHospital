af0a9aec1b61c32e0d1107ca9fc4fd2c8718ff51
elRohit <r.kumar@sapalomera.cat>
2024-05-08 23:28:45 +0200

    dummy v1rep

--- DUMMY DATA/manipulacion_dummy.py
+++ DUMMY DATA/manipulacion_dummy.py
@@ -7,20 +7,22 @@ filearray = []
 
 
 
-def read_csv():
+def import_ciutats():
     #Postgres conn
-    conn = psycopg2.connect(database="hospital",user="postgres",password="P@ssw0rd",host="192.168.56.10",port="5432")
+    conn = psycopg2.connect(database="hospital",user="postgres",password="P@ssw0rd",host="192.168.1.50",port="5432")
     conn.autocommit = True
     cur = conn.cursor()
     ## Legir el fitxer csv i guardar-lo en un array
     with open('DUMMY DATA/espanya.csv', 'r', newline='',encoding='utf-8') as file:
         reader = csv.DictReader(file, delimiter=',')
-        count = 1
+        count = 0
         
         
         print('*'*80)
+        cur.execute(f"DELETE FROM audit.ciutats")
         for row in reader:
             count += 1
+            
             cur.execute(f"INSERT INTO audit.ciutats VALUES ({row['Codi']},'{row['Nom']}', {row['CodiProvíncia']})")
         print(f'total de registres: {count}')
         cur.close()
@@ -28,7 +30,7 @@ def read_csv():
     # Retornar el array
     return filearray
     
-read_csv()
+#import_ciutats()
 
     
     
