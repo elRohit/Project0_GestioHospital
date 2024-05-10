7555db6f45257271e4faf3131831954f18981dbd
elRohit <r.kumar@sapalomera.cat>
2024-05-10 18:33:45 +0200

    Update manipulacion_dummy.py

--- DUMMY DATA/manipulacion_dummy.py
+++ DUMMY DATA/manipulacion_dummy.py
@@ -4,23 +4,13 @@ import psycopg2
 import random
 from faker import Faker
 # Aqui es guardaran totes les dades del fitxer csv
-host_conn = '192.168.1.50'
-faker = Faker()
+host_conn = '10.94.255.130'
+pswd = 'P@ssw0rd'
+faker = Faker('ru_RU')
 
 
 
-def personals():
-    conn = psycopg2.connect(database="hospital",user="postgres",password="P@ssw0rd",host=host_conn,port="5432")
-    conn.autocommit = True
-    cur = conn.cursor()
-    fucker_data = Faker()
-    for fuck in range(150450):
-        cur.execute(f"INSERT INTO personal VALUES ({len(fuck)},{fake.first_name()},{fake.last_name()})")
-'''filearray_ciutats = []
-
-
-
-def import_ciutats():
+'''def import_ciutats():
     #Postgres conn
     conn = psycopg2.connect(database="hospital",user="postgres",password="P@ssw0rd",host=host_conn,port="5432")
     conn.autocommit = True
@@ -43,35 +33,54 @@ def import_ciutats():
         conn.close()
 
         return filearray_ciutats'''
-
-def import_personal():
-    #Faker
-    #Gen Dades De peersoal
-    # Id,dni.nom,cognoms,correu,num_telefon,adreça
-    # Dni
-    random.seed()
-    num_jn = list()
-    ll = random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
-    for i in range(8):
-        i = random.randint(1, 9)
-        num_jn.append(i)
-    dni_final = ''.join(map(str, num_jn)) + ll
+        
+regs_impr = int(input('Quants registres vols introduir a la taula personal?...'))
+def import_personal(regs_impr):
+    
 
 
     #Postgres conn
-    #conn = psycopg2.connect(database="hospital",user="postgres",password="P@ssw0rd",host=host_conn,port="5432")
-    #conn.autocommit = True
-    #cur = conn.cursor()
-    for i in range(0,51):
-        #cur.execute(f"INSERT INTO personal VALUES ({len(i)},{faker.first_name()},{faker.last_name()},{faker.email()},{faker.phone_number()},{faker.address()})")
-        print(f"{(i)},{dni_final},{faker.first_name()},{faker.last_name()},{faker.email()},{faker.phone_number()},{faker.address()}")
+    conn = psycopg2.connect(database="hospital",user="postgres",password=pswd,host=host_conn,port="5432")
+    conn.autocommit = True
+    cur = conn.cursor()
+    
+    count = 0
+    for i in range(0,regs_impr):
+            count += 1
+            random.seed()
+            #dni
+            num_jn = list()
+            ll = random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+            for i in range(8):
+                i = random.randint(1, 9)
+                num_jn.append(i)
+            dni_final = ''.join(map(str, num_jn)) + ll 
+            #num_tel
+            num_tel = list()
+            for i in range(9):
+                i = random.randint(1, 9)
+                num_tel.append(i)
+            num_tel_final = ''.join(map(str, num_tel))
+            cur.execute(f"INSERT INTO personal VALUES ({count},'{dni_final}','{faker.first_name()[:20]}','{faker.last_name()[:20]}','{faker.email()[:20]}',{num_tel_final[:20]},'{faker.address()[:20]}')")
+    print(f'total de registres: {count}')
     #cur.close()
     #conn.close()
     ## Crear un fitxer csv amb dades dummy amb el nom personal.csv i amb modul random
-   
-
-
-import_personal()
+    
+def metges():
+    conn = psycopg2.connect(database="hospital",user="postgres", password=pswd,host=host_conn,port="5432")
+    conn.autocommit = True
+    cur = conn.cursor()
+    count = 0
+    
+    for i in range(50):
+        count += 1
+        o = random.randint(1,101)
+        cur.execute(f"INSERT INTO medicos(p_id,curriculum) VALUES ({o},'{faker.job()[:20]}')")
+        
+#import_personal(regs_impr)
+metges()
+    
 
     
     
