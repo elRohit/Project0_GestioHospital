f738aed2ec54f0a7103f960d483bd2ab17aae005
1smex48 <i.flor@sapalomera.cat>
2024-05-10 16:49:31 +0200

    exportacion de datitos (muy chikitos) mas XMLito

--- BLOC DE PROGRAMACIÓ/exportacio_de_dades.py
+++ BLOC DE PROGRAMACIÓ/exportacio_de_dades.py
@@ -13,7 +13,7 @@ def exportacion_datitos(fecha_inicio, fecha_fin):
         sslmode="require"
     )
     cur = connexio.cursor()
-    SQLita = f"SELECT p.nombre, p.apellidos, pa.id_tarjeta_sanitaria, pa.nombre, pa.apellidos, d.fecha_entrada, d.fecha_salida, d.tiene_receta, d.medicamentos, m.nombre_malaltia FROM diagnosticos d JOIN personal p ON p.p_id = d.p_id JOIN pacientes pa ON pa.id_tarjeta_sanitaria = d.id_tarjeta_sanitaria JOIN malalties m ON m.id_m = d.id_m WHERE fecha_entrada BETWEEN '{fecha_inicio}.000000' AND '{fecha_fin}.999999' ORDER BY fecha_entrada ASC;"
+    SQLita = f"SELECT p.nombre, p.apellidos, pa.id_tarjeta_sanitaria, pa.nombre, pa.apellidos, d.fecha_entrada, d.fecha_salida, d.tiene_receta, d.medicamentos, m.nombre_malaltia, p.dni FROM diagnosticos d JOIN personal p ON p.p_id = d.p_id JOIN pacientes pa ON pa.id_tarjeta_sanitaria = d.id_tarjeta_sanitaria FULL JOIN malalties m ON m.id_m = d.id_m WHERE fecha_entrada BETWEEN '{fecha_inicio}.000000' AND '{fecha_fin}.999999' ORDER BY fecha_entrada ASC;"
     cur.execute(SQLita)
     resultadito = cur.fetchall()
     cur.close()
@@ -23,8 +23,10 @@ def exportacion_datitos(fecha_inicio, fecha_fin):
 def exportacion_xml(fecha_inicio, fecha_fin):
     contador = 0
     datitos = exportacion_datitos(fecha_inicio, fecha_fin)
+    xmlns = "xmlns"
     
-    root = ET.Element(f"visitas")    
+    root = ET.Element("visitas", attrib={"xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance", "xsi:noNamespaceSchemaLocation": "visitesIRA.xsd"})
+    
     
     for datito in datitos:
         ET.indent(root, space="\t", level=0)
@@ -37,44 +39,55 @@ def exportacion_xml(fecha_inicio, fecha_fin):
         numero_diagnostico.text = str(contador)
         ET.indent(diagnostico, space="\t", level=1)
         
-        fecha_diagnostico = ET.SubElement(diagnostico, "nombre_medico")
-        fecha_diagnostico.text = str(datito[0])
+        dni_medico = ET.SubElement(diagnostico, "dni_medico")
+        dni_medico.text = str(datito[10])
         ET.indent(diagnostico, space="\t", level=1)
         
-        paciente = ET.SubElement(diagnostico, "apellidos_medico")
-        paciente.text = str(datito[1])
+        nombre_medico = ET.SubElement(diagnostico, "nombre_medico")
+        nombre_medico.text = str(datito[0])
         ET.indent(diagnostico, space="\t", level=1)
         
-        medico = ET.SubElement(diagnostico, "tarjeta_sanitaria_paciente")
-        medico.text = str(datito[2])
+        apellidos_medico = ET.SubElement(diagnostico, "apellidos_medico")
+        apellidos_medico.text = str(datito[1])
         ET.indent(diagnostico, space="\t", level=1)
         
-        enfermedad = ET.SubElement(diagnostico, "nombre_paciente")
-        enfermedad.text = str(datito[3])
+        tarjeta_sanitaria_paciente = ET.SubElement(diagnostico, "tarjeta_sanitaria_paciente")
+        tarjeta_sanitaria_paciente.text = str(datito[2])
         ET.indent(diagnostico, space="\t", level=1)
         
-        tratamiento = ET.SubElement(diagnostico, "apellidos_paciente")
-        tratamiento.text = str(datito[4])
+        nombre_paciente = ET.SubElement(diagnostico, "nombre_paciente")
+        nombre_paciente.text = str(datito[3])
         ET.indent(diagnostico, space="\t", level=1)
         
-        observaciones = ET.SubElement(diagnostico, "fecha_entrada")
-        observaciones.text = str(datito[5])
+        apellidos_paciente = ET.SubElement(diagnostico, "apellidos_paciente")
+        apellidos_paciente.text = str(datito[4])
         ET.indent(diagnostico, space="\t", level=1)
         
-        observaciones = ET.SubElement(diagnostico, "fecha_salida")
-        observaciones.text = str(datito[6])
+        fecha_entrada = ET.SubElement(diagnostico, "fecha_entrada")
+        fecha_entrada_fecha = str(datito[5])[0:10]
+        fecha_entrada_hora = str(datito[5])[11:19]
+        fecha_entrada.text = str(fecha_entrada_fecha + "T" + fecha_entrada_hora)
         ET.indent(diagnostico, space="\t", level=1)
         
-        observaciones = ET.SubElement(diagnostico, "tiene_receta")
-        observaciones.text = str(datito[7])
+        fecha_salida = ET.SubElement(diagnostico, "fecha_salida")
+        if datito[6] == None:
+            fecha_salida.text = "None"
+        else:
+            fecha_fin_fecha = str(datito[6])[0:10]
+            fecha_fin_hora = str(datito[6])[11:19]
+            fecha_salida.text = str(fecha_fin_fecha + "T" + fecha_fin_hora)
         ET.indent(diagnostico, space="\t", level=1)
         
-        observaciones = ET.SubElement(diagnostico, "medicamentos")
-        observaciones.text = str(datito[8])
+        tiene_receta = ET.SubElement(diagnostico, "tiene_receta")
+        tiene_receta.text = str(datito[7])
         ET.indent(diagnostico, space="\t", level=1)
         
-        observaciones = ET.SubElement(diagnostico, "nombre_enfermedad")
-        observaciones.text = str(datito[9])
+        medicamentos = ET.SubElement(diagnostico, "medicamentos")
+        medicamentos.text = str(datito[8])
+        ET.indent(diagnostico, space="\t", level=1)
+        
+        nombre_enfermedad = ET.SubElement(diagnostico, "nombre_enfermedad")
+        nombre_enfermedad.text = str(datito[9])
         ET.indent(diagnostico, space="\t", level=1)
         
         contador += 1
