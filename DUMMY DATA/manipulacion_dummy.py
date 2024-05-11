ef7c795fb53114c813ab31722396596cc1c0b66a
elRohit <r.kumar@sapalomera.cat>
2024-05-11 08:58:35 +0200

    Update manipulacion_dummy.py

--- DUMMY DATA/manipulacion_dummy.py
+++ DUMMY DATA/manipulacion_dummy.py
@@ -4,7 +4,7 @@ import psycopg2
 import random
 from faker import Faker
 # Aqui es guardaran totes les dades del fitxer csv
-host_conn = '10.94.255.130'
+host_conn = '192.168.1.50'
 pswd = 'P@ssw0rd'
 faker = Faker('ru_RU')
 
@@ -34,8 +34,8 @@ faker = Faker('ru_RU')
 
         return filearray_ciutats'''
         
-regs_impr = int(input('Quants registres vols introduir a la taula personal?...'))
-def import_personal(regs_impr):
+
+def import_personal(personal):
     
 
 
@@ -45,7 +45,7 @@ def import_personal(regs_impr):
     cur = conn.cursor()
     
     count = 0
-    for i in range(0,regs_impr):
+    for i in range(personal):
             count += 1
             random.seed()
             #dni
@@ -61,28 +61,100 @@ def import_personal(regs_impr):
                 i = random.randint(1, 9)
                 num_tel.append(i)
             num_tel_final = ''.join(map(str, num_tel))
-            cur.execute(f"INSERT INTO personal VALUES ({count},'{dni_final}','{faker.first_name()[:20]}','{faker.last_name()[:20]}','{faker.email()[:20]}',{num_tel_final[:20]},'{faker.address()[:20]}')")
-    print(f'total de registres: {count}')
+            cur.execute(f"INSERT INTO personal VALUES ({count},'{dni_final}','{faker.first_name()[:20]}','{faker.last_name()[:20]}','{faker.email()[:50]}',{num_tel_final[:20]},'{faker.address()[:20]}')")
+    print(f'total de registres dels personals: {count}')
     #cur.close()
     #conn.close()
     ## Crear un fitxer csv amb dades dummy amb el nom personal.csv i amb modul random
     
-def metges():
+def metges(metges):
     conn = psycopg2.connect(database="hospital",user="postgres", password=pswd,host=host_conn,port="5432")
     conn.autocommit = True
     cur = conn.cursor()
     count = 0
     
-    for i in range(50):
+    for i in range(metges):
         count += 1
-        o = random.randint(1,101)
-        cur.execute(f"INSERT INTO medicos(p_id,curriculum) VALUES ({o},'{faker.job()[:20]}')")
+        cur.execute(f"INSERT INTO medicos(p_id,curriculum) VALUES ({i+1},'{faker.job()}')")
+    print(f'total de registres dels metges: {count}')
         
-#import_personal(regs_impr)
-metges()
-    
+    cur.close()
+    conn.close()
 
+def enfermers(qty):
+    conn = psycopg2.connect(database="hospital",user="postgres", password=pswd,host=host_conn,port="5432")
+    conn.autocommit = True
+    cur = conn.cursor()
+    count = 0
     
+    for i in range(qty):
+        count += 1
+        p_id = i + 101
+        cur.execute(f"INSERT INTO enfermeros(p_id,experiencia) VALUES ({p_id},'{faker.job()}')")
+    print(f'total de registres de enfermers: {count}')
+        
+    cur.close()
+    conn.close()
+
+def personal_neteja(netejadors):
+    conn = psycopg2.connect(database="hospital",user="postgres", password=pswd,host=host_conn,port="5432")
+    conn.autocommit = True
+    cur = conn.cursor()
+    count = 0
+    for i in range(netejadors):
+        count += 1
+        p_id = i + 301
+        # Уборка номеров significa neteja 
+        cur.execute(f"INSERT INTO varios VALUES ({p_id},'{'Уборка номеров'}')")
+    print(f'total de registres de netejadors: {count}')
+    cur.close()
+    conn.close()
+
+def personal_administracio(administracio):
+    conn = psycopg2.connect(database="hospital",user="postgres", password=pswd,host=host_conn,port="5432")
+    conn.autocommit = True
+    cur = conn.cursor()
+    count = 0
+    for i in range(administracio):
+        count += 1
+        p_id = i + 401
+        # администрация significa administració
+        cur.execute(f"INSERT INTO varios VALUES ({p_id},'{'администрация'}')")
+    print(f'total de registres de administarció: {count}')
+    cur.close()
+    conn.close()
+
+#import_personal(450)
+#metges(100)
+#enfermers(200)
+#personal_neteja(100)
+#personal_administracio(50)
+    '''
+def pacients(pacients):
+    # Volem crear pacients amb dades dummy, 100.000 pacients i per millorar rendiment crearem indexos.
+    conn = psycopg2.connect(database="hospital",user="postgres", password=pswd,host=host_conn,port="5432")
+    conn.autocommit = True
+    cur = conn.cursor()
+    count = 0
+    for i in range(pacients):
+        count += 1
+        cur.execute(f"INSERT INTO pacients VALUES ({count},'{faker.first_name()[:20]}','{faker.last_name()[:20]}','{faker.email()[:50]}','{faker.address()[:20]}')")
+    print(f'total de registres dels pacients: {count}')
+    '''
+
+def visites(visites):
+    conn = psycopg2.connect(database="hospital",user="postgres", password=pswd,host=host_conn,port="5432")
+    conn.autocommit = True
+    cur = conn.cursor()
+    count = 0
+    for i in range(visites):
+        count += 1
+        cur.execute(f"INSERT INTO visites VALUES ({count},'{faker.date_between(start_date='-1y', end_date='today')}','{faker.time()}','{random.randint(1, 100)}','{random.randint(1, 100)}')")
+    print(f'total de registres de visites: {count}')
+    cur.close()
+    conn.close()
+
+
     
     
 
