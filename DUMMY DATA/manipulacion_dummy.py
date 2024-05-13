4b8b87e2f4aca8e8d4642453005cf330d259079c
elRohit <r.kumar@sapalomera.cat>
2024-05-13 17:36:01 +0200

    Update manipulacion_dummy.py

--- DUMMY DATA/manipulacion_dummy.py
+++ DUMMY DATA/manipulacion_dummy.py
@@ -7,38 +7,25 @@ import string
 import time
 
 # Aqui es guardaran totes les dades del fitxer csv
-host_conn = '192.168.1.50'
+host_conn = '192.168.56.10'
 pswd = 'P@ssw0rd'
 faker = Faker('ru_RU')
 
-
-
-'''def import_ciutats():
-    #Postgres conn
-    conn = psycopg2.connect(database="hospital",user="postgres",password="P@ssw0rd",host=host_conn,port="5432")
+def clean_all():
+    conn = psycopg2.connect(database="hospital",user="postgres",password=pswd,host=host_conn,port="5432")
     conn.autocommit = True
     cur = conn.cursor()
-    ## Legir el fitxer csv i guardar-lo en un array
-    with open('DUMMY DATA/espanya.csv', 'r', newline='',encoding='utf-8') as file:
-        reader = csv.DictReader(file, delimiter=',')
-        count = 0
-        
-        
-        print('*'*80)
-        cur.execute(f"DELETE FROM audit.ciutats")
-        for row in reader:
-            filearray_ciutats.append(row)
-            count += 1
-            
-            cur.execute(f"INSERT INTO audit.ciutats VALUES ({row['Codi']},'{row['Nom']}', {row['CodiProvíncia']})")
-        print(f'total de registres: {count}')
-        cur.close()
-        conn.close()
-
-        return filearray_ciutats'''
-        
-
-def import_personal(personal):
+    cur.execute("DELETE FROM diagnosticos")
+    cur.execute("DELETE FROM enfermeros")
+    cur.execute("DELETE FROM medicos")
+    cur.execute("DELETE FROM varios")
+    cur.execute("DELETE FROM pacientes")
+    cur.execute("DELETE FROM personal")
+    
+    cur.close()
+    conn.close()
+    
+def personal(personal):
     
     #Postgres conn
     conn = psycopg2.connect(database="hospital",user="postgres",password=pswd,host=host_conn,port="5432")
@@ -67,8 +54,7 @@ def import_personal(personal):
     cur.close()
     conn.close()
 
-    
-def metges(metges):
+def medicos(metges):
     conn = psycopg2.connect(database="hospital",user="postgres", password=pswd,host=host_conn,port="5432")
     conn.autocommit = True
     cur = conn.cursor()
@@ -82,7 +68,7 @@ def metges(metges):
     cur.close()
     conn.close()
 
-def enfermers(qty):
+def enfermeros(qty):
     conn = psycopg2.connect(database="hospital",user="postgres", password=pswd,host=host_conn,port="5432")
     conn.autocommit = True
     cur = conn.cursor()
@@ -97,7 +83,7 @@ def enfermers(qty):
     cur.close()
     conn.close()
 
-def personal_neteja(netejadors):
+def personal_limpieza(netejadors):
     conn = psycopg2.connect(database="hospital",user="postgres", password=pswd,host=host_conn,port="5432")
     conn.autocommit = True
     cur = conn.cursor()
@@ -124,12 +110,6 @@ def personal_administracio(administracio):
     print(f'total de registres de administarció: {count}')
     cur.close()
     conn.close()
-
-#import_personal(450)
-#metges(100)
-#enfermers(200)
-#personal_neteja(100)
-#personal_administracio(50)
 
 def show_loader():
                 for i in range(101):
@@ -174,27 +154,73 @@ def pacients(pacients):
             
             count += 1
             cur.execute(f"INSERT INTO pacientes (id_tarjeta_sanitaria, nombre, apellidos, fecha_nacimiento, direccion, num_telefono, contacto_emergencia, condiciones_paciente) VALUES ('{tse}','{inicials_nom}','{inicials_cognom}','{data_naix}','{faker.address()}',{num_tel_final},NULL,True) ON CONFLICT DO NOTHING")
+            print(f'{count} pacient/s creats')
         show_loader()
             
         cur.close()
         conn.close()
 
-
-pacients(100)
-
-'''def visites(visites):
+def visites(visites):
+    count = 0
     conn = psycopg2.connect(database="hospital",user="postgres", password=pswd,host=host_conn,port="5432")
     conn.autocommit = True
     cur = conn.cursor()
-    count = 0
-    for i in range(visites):
-        count += 1
-        cur.execute(f"INSERT INTO reserves VALUES ({count},'{faker.date_time_this_year()}','{faker.date_time_this_year()}'")
-    print(f'total de registres de visites: {count}')
+    cur.execute(f"SELECT p_id FROM medicos")
+    rows = cur.fetchall()
+    pers_ids = list()
+    
+    for row in rows:
+        pers_ids.append(row[0])
+    
     cur.close()
-    conn.close()'''
-
+        
+    cur = conn.cursor()
+    pacients_tse = list()
+    cur.execute(f"SELECT id_tarjeta_sanitaria FROM pacientes")
+    rows2 = cur.fetchall()
+    for row1 in rows2:
+        pacients_tse.append(row1[0])
 
     
+    for _ in range(visites):
+        count += 1
+        # Crear visites
+        data = faker.date_of_birth()
+        ts_entrada = faker.date() + ' ' + faker.time()
+        ts_sortida = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.mktime(time.strptime(ts_entrada, '%Y-%m-%d %H:%M:%S')) + 1800))
+        p_id = random.choice(pers_ids)
+        tse = random.choice(pacients_tse)
+        medicamentos = ['Paracetamol', 'Ibuprofeno', 'Aspirina', 'Amoxicilina', 'Omeprazol', 'Diazepam', 'Lorazepam', 'Clonazepam', 'Alprazolam', 'Zolpidem', 'Zopiclona', 'Trankimazin', 'Rivotril', 'Lexatin', 'Valium', 'Xanax', 'Orfidal', 'Stilnox', 'Imovane', 'Alplax', 'Dormidina', 'Dormicum']
+        cur.execute(f"INSERT INTO diagnosticos (p_id, id_tarjeta_sanitaria,fecha_entrada,fecha_salida,tiene_receta,medicamentos) VALUES ('{p_id}','{tse}','{ts_entrada}','{ts_sortida}','Si','{random.choice(medicamentos)}')")
+    print(f'total de registres de visites: {count}')
+    
+    cur.close()
+    conn.close()
+
+def indexos():
+    psycopg2.connect(database="hospital",user="postgres", password=pswd,host=host_conn,port="5432")
+    conn.autocommit = True
+    cur = conn.cursor()
+    cur.execute("CREATE INDEX idx_pacientes ON pacientes (id_tarjeta_sanitaria)")
+    cur.execute("CREATE INDEX idx_personal ON personal (dni)")
+    cur.execute("CREATE INDEX idx_medicos ON medicos (p_id)")
+    cur.execute("CREATE INDEX idx_enfermeros ON enfermeros (p_id)")
+    cur.execute("CREATE INDEX idx_personal_limpieza ON varios (p_id)")
+    cur.execute("CREATE INDEX idx_personal_administracion ON varios (p_id)")
+    cur.execute("CREATE INDEX idx_diagnosticos ON diagnosticos (p_id)")
+    cur.close()
+    conn.close()
+    
+    
+    
+    
+    
+# Execs
+
+
+#medicos(100)
+#enfermeros(50)
+visites(50)
+
 
 
