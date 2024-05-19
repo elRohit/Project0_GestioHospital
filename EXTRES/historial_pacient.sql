0854a4d25038b1050e9dea04070298d65b350e9c
elRohit <r.kumar@sapalomera.cat>
2024-05-19 20:35:48 +0200

    ampiacio

---
+++ EXTRES/historial_pacient.sql
@@ -0,0 +1,25 @@
+CREATE TABLE historial_pacient (
+    id INT PRIMARY KEY,
+    pacient_id INT,
+    visita_date DATE,
+    intervencio VARCHAR(255),
+    pla_medicacio VARCHAR(255),
+    FOREIGN KEY (pacient_id) REFERENCES pacients(id)
+);
+
+-- Crear la funció per a insertar un nou historial
+
+CREATE OR REPLACE FUNCTION insert_historial_pacient()
+RETURNS TRIGGER AS $$
+BEGIN
+    INSERT INTO historial_pacient (id, pacient_id, visita_date, diagnostic, intervencio, pla_medicacio)
+    VALUES (NEW.p_id, NEW.id_tarjeta_sanitaria, NEW.fecha_entrada, NEW.diagnostic, NEW.-, NEW.medicamentos);
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Crear el trigger per a insertar un nou historial
+CREATE TRIGGER insert_historial_pacient
+AFTER INSERT ON pacientes
+FOR EACH ROW
+EXECUTE FUNCTION insert_historial_pacient();
