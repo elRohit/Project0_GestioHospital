a46ffdd7c6608c5ea23577346784d893e40e66a4
elRohit <r.kumar@sapalomera.cat>
2024-05-23 00:40:57 +0200

    Update anon.sql

--- ESQUEMA DE SEGURETAT/DATA_MASKING/anon.sql
+++ ESQUEMA DE SEGURETAT/DATA_MASKING/anon.sql
@@ -5,12 +5,9 @@ SELECT anon.start_dynamic_masking();
 -- SECURITY DEFINER
 
 -- personal (Anonitzar per el metge)
-CREATE ROLE metge WITH LOGIN PASSWORD 'P@ssw0rd' IN ROLE medico;
-
-SECURITY LABEL FOR anon ON ROLE metge IS 'MASKED';
 SECURITY LABEL FOR anon ON COLUMN public.personal.direccion IS 'MASKED WITH FUNCTION anon.random_address()';
 SECURITY LABEL FOR anon ON COLUMN public.personal.num_telefono IS 'MASKED WITH FUNCTION anon.partial(num_telefono, 2,$$*****$$,2)';
-SECURITY LABEL FOR anon ON COLUMN public.personal.email IS 'MASKED WITH VALUE "FAKE_EMAIL" ';
+--SECURITY LABEL FOR anon ON COLUMN public.personal.email IS 'MASKED WITH VALUE "FAKE_EMAIL" ';
 
 --Anonimitzar una taula
 SELECT anon.anonymize_table('personal');
